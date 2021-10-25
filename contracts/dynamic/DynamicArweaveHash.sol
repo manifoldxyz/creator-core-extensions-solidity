5e9475d23b111bdac2f25b50b576b9f909b15c62
Wilkins Chung <wilkins.chung@gmail.com>
2021-10-24 18:32:12 -0700

    refactor DynamicArweaveHash and add TimeToken

--- contracts/dynamic/DynamicArweaveHash.sol
+++ contracts/dynamic/DynamicArweaveHash.sol
@@ -10,24 +10,35 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@manifoldxyz/creator-core-solidity/contracts/core/IERC721CreatorCore.sol";
 import "@manifoldxyz/creator-core-solidity/contracts/extensions/CreatorExtension.sol";
 import "@manifoldxyz/creator-core-solidity/contracts/extensions/ICreatorExtensionTokenURI.sol";
-import "@manifoldxyz/creator-core-solidity/contracts/extensions/ERC721/IERC721CreatorExtensionApproveTransfer.sol";
 
 import "../libraries/SingleCreatorExtension.sol";
 
-abstract contract DynamicArweaveHash is ERC721SingleCreatorExtension, CreatorExtension, Ownable, ICreatorExtensionTokenURI, IERC721CreatorExtensionApproveTransfer {
+abstract contract DynamicArweaveHash is ERC721SingleCreatorExtension, CreatorExtension, Ownable, ICreatorExtensionTokenURI {
 
     using Strings for uint256;
 
-    string[] public arweaveHashes;
+    string[] public imageArweaveHashes;
+    string[] public animationArweaveHashes;
 
     function supportsInterface(bytes4 interfaceId) public view virtual override(CreatorExtension, IERC165) returns (bool) {
         return interfaceId == type(ICreatorExtensionTokenURI).interfaceId 
-        || interfaceId == type(IERC721CreatorExtensionApproveTransfer).interfaceId
         || super.supportsInterface(interfaceId);
     }
 
     function mint(address to) public virtual onlyOwner returns(uint256) {
         return IERC721CreatorCore(_creator).mintExtension(to);
+    }
+
+    function tokenURI(address, uint256 tokenId) external view virtual override returns (string memory) {
+        string memory uri = string(abi.encodePacked('data:application/json;utf8,{"name":"', _getName(), '","description":"', _getDescription()));
+        if (imageArweaveHashes.length > 0) {
+            uri = string(abi.encodePacked(uri, '", "image":"https://arweave.net/', _getImageHash(tokenId)));
+        }
+        if (animationArweaveHashes.length > 0) {
+            uri = string(abi.encodePacked(uri, '", "animation_url":"https://arweave.net/', _getAnimationHash(tokenId)));
+        }
+        uri = string(abi.encodePacked(uri, '"}'));
+        return uri;
     }
 
     function _getName() internal view virtual returns(string memory);
@@ -36,5 +47,13 @@ abstract contract DynamicArweaveHash is ERC721SingleCreatorExtension, CreatorExt
 
     function _getImageHash(uint256 tokenId) internal view virtual returns(string memory);
 
-    function setArweaveHashes(string[] memory _arweaveHashes) external virtual;
+    function _getAnimationHash(uint256 tokenId) internal view virtual returns(string memory);
+
+    function setImageArweaveHashes(string[] memory _arweaveHashes) external virtual {
+        imageArweaveHashes = _arweaveHashes;
+    }
+
+    function setAnimationAreaveHashes(string[] memory _arweaveHashes) external virtual {
+        animationArweaveHashes = _arweaveHashes;
+    }
 }
---
+++ contracts/dynamic/TimeToken.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/// @author: manifold.xyz
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+
+import "./DynamicArweaveHash.sol";
+
+/**
+ * A token that changes with time
+ */
+contract TimeToken is DynamicArweaveHash {
+
+    // The image cycle cadence
+    uint256 private _cadence;
+    // The token name
+    string private _name;
+    // The token description
+    string private _description;
+    // Token creation timestamp (used to calculate cycle)
+    uint256 private _creationTimestamp;
+    // Minted token id
+    uint256 private _tokenId;
+
+    constructor(address creator, string memory name, string memory description, uint256 cadence) ERC721SingleCreatorExtension(creator) {
+        _creationTimestamp = block.timestamp;
+        _cadence = cadence;
+        _name = name;
+        _description = description;
+    }
+
+    function mint(address to) public virtual override onlyOwner returns(uint256) {
+        require(_tokenId == 0, "Already minted");
+        _tokenId = super.mint(to);
+        return _tokenId;
+    }
+
+    function _getName() internal view virtual override returns(string memory) {
+        return _name;
+    }
+
+    function _getDescription() internal view virtual override returns(string memory) {
+        return _description;
+    }
+
+    function _getImageHash(uint256) internal view override returns(string memory) {
+        return imageArweaveHashes[(block.timestamp - _creationTimestamp)/_cadence % imageArweaveHashes.length];
+    }
+
+    function _getAnimationHash(uint256) internal view override returns(string memory) {
+        return animationArweaveHashes[(block.timestamp - _creationTimestamp)/_cadence % animationArweaveHashes.length];
+    }
+
+}
