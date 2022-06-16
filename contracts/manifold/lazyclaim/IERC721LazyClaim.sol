c4a8c00971cf04161b05b269a93e67aba1aa9466
Jack Bonaguro <15988128+jackbonaguro@users.noreply.github.com>
2022-06-16 15:57:25 -0400

    Wrote erc721 lazyMint contract and test file (#12)
    * Wrote erc721 lazyMint contract and test file
    
    * Working and tested extension according to talks
    
    * wip extension uris
    
    * Extension token uris working
    
    * Optimization: don't create listing in memory
    
    * Add test for dynamic token URIs
    
    * Feedback: name changes, add override method + test
    
    * Support optional (0) values on merkle trees
    
    * wip
    
    * wip All feedback but tests broken
    
    * More feedback
    
    * More feedback
    
    * Testnet feedback; tokenIds start at 1, gas tests
    
    * Move directories under 'manifold'
    
    * Remove build files
    
    * wip strange test failure
    
    * Fixed wallet minted check & functionality test
    
    * change walletMinted value type to prevent cast op
    
    * switch back to npm from yarn
    
    * fix tests
    
    * structure and gas optimizations. Fix unit tests
    
    * fix tokenURI rendering and fix tests
    
    * save gas by using uint256 keys/values in mappings (3k gas savings overall)
    
    * fix comment
    
    * efficiency and test updates
    
    * clean up docs
    
    * update comments
    
    * fix var name and make gas efficiency change
    
    * remove canMint. add checkMintIndex and getTotalMints
    
    * change event signature type
    
    * simplify mint event
    
    * remove totalMax restriction
    
    * allow modification of totalMax (but can only increase)
    
    * add checkMintIndices
    
    * add mintBatch
    
    * add unit test and fix bug with non-merkle batch mint
    
    * fix bug with index tracking
    
    Co-authored-by: Wilkins Chung <wilkins.chung@gmail.com>

---
+++ contracts/manifold/lazyclaim/IERC721LazyClaim.sol
@@ -0,0 +1,119 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/// @author: manifold.xyz
+
+/**
+ * Lazy Claim interface
+ */
+interface IERC721LazyClaim {
+    enum StorageProtocol { INVALID, NONE, ARWEAVE, IPFS }
+
+    struct ClaimParameters {
+        uint32 totalMax;
+        uint32 walletMax;
+        uint48 startDate;
+        uint48 endDate;
+        StorageProtocol storageProtocol;
+        bool identical;
+        bytes32 merkleRoot;
+        string location;
+    }
+
+    struct Claim {
+        uint32 total;
+        uint32 totalMax;
+        uint32 walletMax;
+        uint48 startDate;
+        uint48 endDate;
+        StorageProtocol storageProtocol;
+        bool identical;
+        bytes32 merkleRoot;
+        string location;
+    }
+
+    event ClaimInitialized(address indexed creatorContract, uint224 indexed claimIndex, address initializer);
+    event ClaimMint(address indexed creatorContract, uint256 indexed claimIndex);
+    event ClaimMintBatch(address indexed creatorContract, uint256 indexed claimIndex, uint16 mintCount);
+
+    /**
+     * @notice initialize a new claim, emit initialize event, and return the newly created index
+     * @param creatorContractAddress    the creator contract the claim will mint tokens for
+     * @param claimParameters           the parameters which will affect the minting behavior of the claim
+     * @return                          the index of the newly created claim
+     */
+    function initializeClaim(address creatorContractAddress, ClaimParameters calldata claimParameters) external returns(uint256);
+
+    /**
+     * @notice update an existing claim at claimIndex
+     * @param creatorContractAddress    the creator contract corresponding to the claim
+     * @param claimIndex                the index of the claim in the list of creatorContractAddress' _claims
+     * @param claimParameters           the parameters which will affect the minting behavior of the claim
+     */
+    function updateClaim(address creatorContractAddress, uint256 claimIndex, ClaimParameters calldata claimParameters) external;
+
+/**
+     * @notice get the number of _claims initialized for a given creator contract
+     * @param creatorContractAddress    the address of the creator contract
+     * @return                          the number of _claims initialized for this creator contract
+     */
+    function getClaimCount(address creatorContractAddress) external view returns(uint256);
+
+    /**
+     * @notice get a claim corresponding to a creator contract and index
+     * @param creatorContractAddress    the address of the creator contract
+     * @param claimIndex                the index of the claim
+     * @return                          the claim object
+     */
+    function getClaim(address creatorContractAddress, uint256 claimIndex) external view returns(Claim memory);
+
+    /**
+     * @notice check if a mint index has been consumed or not (only for merkle claims)
+     *
+     * @param creatorContractAddress    the address of the creator contract for the claim
+     * @param claimIndex                the index of the claim
+     * @param mintIndex                 the mint index of the claim
+     * @return                          whether or not the mint index was consumed
+     */
+    function checkMintIndex(address creatorContractAddress, uint256 claimIndex, uint32 mintIndex) external view returns(bool);
+
+    /**
+     * @notice check if multiple mint indices has been consumed or not (only for merkle claims)
+     *
+     * @param creatorContractAddress    the address of the creator contract for the claim
+     * @param claimIndex                the index of the claim
+     * @param mintIndices               the mint index of the claim
+     * @return                          whether or not the mint index was consumed
+     */
+    function checkMintIndices(address creatorContractAddress, uint256 claimIndex, uint32[] calldata mintIndices) external view returns(bool[] memory);
+
+    /**
+     * @notice get mints made for a wallet (only for non-merkle claims with walletMax)
+     *
+     * @param minter                    the address of the minting address
+     * @param creatorContractAddress    the address of the creator contract for the claim
+     * @param claimIndex                the index of the claim
+     * @return                          how many mints the minter has made
+     */
+    function getTotalMints(address minter, address creatorContractAddress, uint256 claimIndex) external view returns(uint32);
+
+    /**
+     * @notice allow a wallet to lazily claim a token according to parameters
+     * @param creatorContractAddress    the creator contract address
+     * @param claimIndex                the index of the claim for which we will mint
+     * @param mintIndex                 the mint index (only needed for merkle claims)
+     * @param merkleProof               if the claim has a merkleRoot, verifying merkleProof ensures that address + minterValue was used to construct it  (only needed for merkle claims)
+     */
+    function mint(address creatorContractAddress, uint256 claimIndex, uint32 mintIndex, bytes32[] calldata merkleProof) external;
+
+    /**
+     * @notice allow a wallet to lazily claim a token according to parameters
+     * @param creatorContractAddress    the creator contract address
+     * @param claimIndex                the index of the claim for which we will mint
+     * @param mintCount                 the number of claims to mint
+     * @param mintIndices               the mint index (only needed for merkle claims)
+     * @param merkleProofs              if the claim has a merkleRoot, verifying merkleProof ensures that address + minterValue was used to construct it  (only needed for merkle claims)
+     */
+    function mintBatch(address creatorContractAddress, uint256 claimIndex, uint16 mintCount, uint32[] calldata mintIndices, bytes32[][] calldata merkleProofs) external;
+}
