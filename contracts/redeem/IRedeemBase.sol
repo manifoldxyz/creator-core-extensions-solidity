a3a4ba2c92cf140b9018f5cc11f23a9a8cba58ae
Wilkins Chung <wilkins.chung@gmail.com>
2022-11-22 22:03:42 -0800

    add ability to retreeve redeem configuration from the blockchain

--- contracts/redeem/IRedeemBase.sol
+++ contracts/redeem/IRedeemBase.sol
@@ -11,6 +11,11 @@ import "@manifoldxyz/libraries-solidity/contracts/access/IAdminControl.sol";
  */
 interface IRedeemBase is IAdminControl {
 
+    struct TokenRange{
+        uint256 min;
+        uint256 max;
+    }
+
     event UpdateApprovedContracts(address[] contracts, bool[] approved);
     event UpdateApprovedTokens(address contract_, uint256[] tokenIds, bool[] approved);
     event UpdateApprovedTokenRanges(address contract_, uint256[] minTokenIds, uint256[] maxTokenIds);
@@ -21,15 +26,30 @@ interface IRedeemBase is IAdminControl {
     function updateApprovedContracts(address[] calldata contracts, bool[] calldata approved) external;
 
     /**
+     * @dev Get array of approved contracts that can be used to redeem.
+     */
+    function getApprovedContracts() external view returns(address[] memory);
+
+    /**
      * @dev Update approved tokens that can be used to redeem. Can only be called by contract owner/admin.
      */
     function updateApprovedTokens(address contract_, uint256[] calldata tokenIds, bool[] calldata approved) external;
+
+    /**
+     * @dev Get all approved tokens for every contract that can be used to redeem.
+     */
+    function getApprovedTokens() external view returns(address[] memory, uint256[][] memory);
 
     /**
      * @dev Update approved token ranges that can be used to redeem. Can only be called by contract owner/admin.
      * Clears out old ranges
      */
     function updateApprovedTokenRanges(address contract_, uint256[] calldata minTokenIds, uint256[] calldata maxTokenIds) external;
+
+    /**
+     * @dev Get all approved token ranges for every contract that can be used to redeem.
+     */
+    function getApprovedTokenRanges() external view returns(address[] memory, TokenRange[][] memory);
 
     /**
      * @dev Check if an NFT is redeemable
--- contracts/redeem/RedeemBase.sol
+++ contracts/redeem/RedeemBase.sol
@@ -11,23 +11,22 @@ import "@manifoldxyz/libraries-solidity/contracts/access/AdminControl.sol";
 
 import "./IRedeemBase.sol";
 
-struct range{
-   uint256 min;
-   uint256 max;
-}
 
 /**
  * @dev Burn NFT's to receive another lazy minted NFT
  */
 abstract contract RedeemBase is AdminControl, IRedeemBase {
-     using EnumerableSet for EnumerableSet.UintSet;
+    using EnumerableSet for EnumerableSet.UintSet;
+    using EnumerableSet for EnumerableSet.AddressSet;
 
-     // approved contract tokens
-    mapping(address => bool) internal _approvedContracts;
+    // approved contract tokens
+    EnumerableSet.AddressSet internal _approvedContracts;
 
     // approved specific tokens
+    EnumerableSet.AddressSet internal _approvedTokensContracts;
     mapping(address => EnumerableSet.UintSet) internal _approvedTokens;
-    mapping(address => range[]) internal _approvedTokenRange;
+    EnumerableSet.AddressSet internal _approvedTokenRangeContracts;
+    mapping(address => IRedeemBase.TokenRange[]) internal _approvedTokenRange;
      
     /**
      * @dev See {IERC165-supportsInterface}.
@@ -43,9 +42,23 @@ abstract contract RedeemBase is AdminControl, IRedeemBase {
     function updateApprovedContracts(address[] memory contracts, bool[] memory approved) public virtual override adminRequired {
         require(contracts.length == approved.length, "Redeem: Invalid input parameters");
         for (uint i=0; i < contracts.length; i++) {
-            _approvedContracts[contracts[i]] = approved[i];
+            if (approved[i]) {
+                _approvedContracts.add(contracts[i]);
+            } else {
+                _approvedContracts.remove(contracts[i]);
+            }
         }
         emit UpdateApprovedContracts(contracts, approved);
+    }
+
+    /**
+     * @dev See {IRedeemBase-getApprovedContracts}
+     */
+    function getApprovedContracts() public virtual view override returns (address[] memory contracts) {
+        contracts = new address[](_approvedContracts.length());
+        for (uint i=0; i < _approvedContracts.length(); i++) {
+            contracts[i] = _approvedContracts.at(i);
+        }
     }
     
     /**
@@ -53,7 +66,6 @@ abstract contract RedeemBase is AdminControl, IRedeemBase {
      */
     function updateApprovedTokens(address contract_, uint256[] memory tokenIds, bool[] memory approved) public virtual override adminRequired {
         require(tokenIds.length == approved.length, "Redeem: Invalid input parameters");
-
         for (uint i=0; i < tokenIds.length; i++) {
             if (approved[i] && !_approvedTokens[contract_].contains(tokenIds[i])) {
                 _approvedTokens[contract_].add(tokenIds[i]);
@@ -61,7 +73,29 @@ abstract contract RedeemBase is AdminControl, IRedeemBase {
                 _approvedTokens[contract_].remove(tokenIds[i]);
             }
         }
+        if (_approvedTokens[contract_].length() > 0) {
+            _approvedTokensContracts.add(contract_);
+        } else {
+            _approvedTokensContracts.remove(contract_);
+        }
         emit UpdateApprovedTokens(contract_, tokenIds, approved);
+    }
+
+    /**
+     * @dev See {IRedeemBase-getApprovedTokens}
+     */
+    function getApprovedTokens() public virtual view override returns(address[] memory contracts, uint256[][] memory tokenIds) {
+        contracts = new address[](_approvedTokensContracts.length());
+        tokenIds = new uint[][](_approvedTokensContracts.length());
+        for (uint i=0; i < _approvedTokensContracts.length(); i++) {
+            address contract_ = _approvedTokensContracts.at(i);
+            contracts[i] = contract_;
+            EnumerableSet.UintSet storage approvedTokens = _approvedTokens[contract_];
+            tokenIds[i] = new uint[](approvedTokens.length());
+            for (uint j=0; j < approvedTokens.length(); j++) {
+                tokenIds[i][j] = approvedTokens.at(j);
+            }
+        }
     }
 
     /**
@@ -72,16 +106,34 @@ abstract contract RedeemBase is AdminControl, IRedeemBase {
         delete _approvedTokenRange[contract_];
         for (uint i=0; i < minTokenIds.length; i++) {
             require(minTokenIds[i] < maxTokenIds[i], "Redeem: min must be less than max");
-            _approvedTokenRange[contract_].push(range(minTokenIds[i], maxTokenIds[i]));
+            _approvedTokenRange[contract_].push(IRedeemBase.TokenRange(minTokenIds[i], maxTokenIds[i]));
+        }
+        if (minTokenIds.length > 0) {
+            _approvedTokenRangeContracts.add(contract_);
+        } else {
+            _approvedTokenRangeContracts.remove(contract_);
         }
         emit UpdateApprovedTokenRanges(contract_, minTokenIds, maxTokenIds);
+    }
+    
+    /**
+     * @dev See {IRedeemBase-getApprovedTokenRanges}
+     */
+    function getApprovedTokenRanges() public virtual view override returns(address[] memory contracts, IRedeemBase.TokenRange[][] memory tokenRanges) {
+        contracts = new address[](_approvedTokenRangeContracts.length());
+        tokenRanges = new IRedeemBase.TokenRange[][](_approvedTokenRangeContracts.length());
+        for (uint i=0; i < _approvedTokenRangeContracts.length(); i++) {
+            address contract_ = _approvedTokenRangeContracts.at(i);
+            contracts[i] = contract_;
+            tokenRanges[i] = _approvedTokenRange[contract_];
+        }
     }
 
     /**
      * @dev See {IRedeemBase-redeemable}
      */    
     function redeemable(address contract_, uint256 tokenId) public view virtual override returns(bool) {
-         if (_approvedContracts[contract_]) {
+         if (_approvedContracts.contains(contract_)) {
              return true;
          }
          if (_approvedTokens[contract_].contains(tokenId)) {
