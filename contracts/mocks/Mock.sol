f298d7f551e0e2e02b7b16f4f0ba007438fd147e
jtgi <j@jtgi.me>
2022-12-01 21:28:25 +0900

    test: operator filter integration tests

--- contracts/mocks/Mock.sol
+++ contracts/mocks/Mock.sol
@@ -11,47 +11,46 @@ import "../enumerable/ERC721/ERC721OwnerEnumerableExtension.sol";
 import "../enumerable/ERC721/ERC721OwnerEnumerableSingleCreatorExtension.sol";
 import "../redeem/ERC721/ERC721RedeemBase.sol";
 
-// contract MockERC721Creator is ERC721Creator {
-//      constructor (string memory _name, string memory _symbol) ERC721Creator(_name, _symbol) {}
-// }
+contract MockERC721Creator is ERC721Creator {
+     constructor (string memory _name, string memory _symbol) ERC721Creator(_name, _symbol) {}
+}
 
-// contract MockERC1155Creator is ERC1155Creator {
-//      constructor () ERC1155Creator("gm", "gm") {}
-// }
+contract MockERC1155Creator is ERC1155Creator {
+     constructor () ERC1155Creator("gm", "gm") {}
+}
 
-// contract MockERC721OwnerEnumerableExtension is ERC721OwnerEnumerableExtension {
-//     function testMint(address creator, address to) public {
-//         ERC721Creator(creator).mintExtension(to);
-//     }
-// }
+contract MockERC721OwnerEnumerableExtension is ERC721OwnerEnumerableExtension {
+    function testMint(address creator, address to) public {
+        ERC721Creator(creator).mintExtension(to);
+    }
+}
 
-// contract MockERC721OwnerEnumerableSingleCreatorExtension is ERC721OwnerEnumerableSingleCreatorExtension {
-//     constructor(address creator) ERC721OwnerEnumerableSingleCreatorExtension(creator) {}
+contract MockERC721OwnerEnumerableSingleCreatorExtension is ERC721OwnerEnumerableSingleCreatorExtension {
+    constructor(address creator) ERC721OwnerEnumerableSingleCreatorExtension(creator) {}
 
-//     function testMint(address to) public {
-//         ERC721Creator(_creator).mintExtension(to);
-//     }
-// }
+    function testMint(address to) public {
+        ERC721Creator(_creator).mintExtension(to);
+    }
+}
 
-// contract MockERC721RedeemEnumerable is ERC721OwnerEnumerableSingleCreatorBase, ERC721RedeemBase {
-//     constructor(address creator, uint16 redemptionRate_, uint16 redemptionMax_) ERC721RedeemBase(creator, redemptionRate_, redemptionMax_) {}
+contract MockERC721RedeemEnumerable is ERC721OwnerEnumerableSingleCreatorBase, ERC721RedeemBase {
+    constructor(address creator, uint16 redemptionRate_, uint16 redemptionMax_) ERC721RedeemBase(creator, redemptionRate_, redemptionMax_) {}
 
-//     function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721RedeemBase, ERC721CreatorExtensionApproveTransfer) returns (bool) {
-//         return ERC721RedeemBase.supportsInterface(interfaceId) || ERC721CreatorExtensionApproveTransfer.supportsInterface(interfaceId);
-//     }
-// }
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721RedeemBase, ERC721CreatorExtensionApproveTransfer) returns (bool) {
+        return ERC721RedeemBase.supportsInterface(interfaceId) || ERC721CreatorExtensionApproveTransfer.supportsInterface(interfaceId);
+    }
+}
 
-// contract MockETHReceiver {
-//     fallback() external payable {
-//         // Transfer caps gas at 2300. This function needs to consume more gas than that.
-//         for (uint j = 0; j < 2300;) {
-//             unchecked{ j++; }
-//         }
-//     }
-// }
+contract MockETHReceiver {
+    fallback() external payable {
+        // Transfer caps gas at 2300. This function needs to consume more gas than that.
+        for (uint j = 0; j < 2300;) {
+            unchecked{ j++; }
+        }
+    }
+}
 
 contract MockRegistry {
-    error OperatorNotAllowed(address operator);
     address[] public _failOperators;
 
     function setBlockedOperators(address[] memory failOperators) public {
--- test/manifold/operatorfilterer.js
+++ test/manifold/operatorfilterer.js
@@ -1,7 +1,7 @@
 const truffleAssert = require('truffle-assertions');
 const OperatorFilter = artifacts.require("OperatorFilterer");
-const ERC721Creator = artifacts.require('@manifoldxyz/creator-core-solidity/MockERC721');
-const ERC1155Creator = artifacts.require('@manifoldxyz/creator-core-solidity/MockERC1155');
+const ERC721Creator = artifacts.require('@manifoldxyz/creator-core-solidity/ERC721Creator.sol');
+const ERC1155Creator = artifacts.require('@manifoldxyz/creator-core-solidity/ERC1155Creator.sol');
 const MockRegistry = artifacts.require("MockRegistry");
 const tokenURI = "https://example.com";
 
@@ -9,81 +9,81 @@ contract('OperatorFilterer', function ([...accounts]) {
   const [owner, operator, operator2, anyone1] = accounts;
 
   describe("ERC721", () => {
-    let creator, mock, ext;
+    let creator, registry, ext;
 
     beforeEach(async () => {
       creator = await ERC721Creator.new("gm", "GM", { from: owner, gas: 9000000 });
-      mock = await MockRegistry.new({ from: owner });
-      ext = await OperatorFilter.new(mock.address, '0x000000000000000000000000000000000000dEaD', false, { from: owner });
+      registry = await MockRegistry.new({ from: owner });
+      ext = await OperatorFilter.new(registry.address, '0x000000000000000000000000000000000000dEaD', { from: owner });
+
+      await creator.setApproveTransfer(ext.address);
+    });
+
+    it('should allow operators not on filtered list', async () => {
+      await registry.setBlockedOperators([operator2 /* <-- not `operator` */], { from: owner });
+      await creator.mintBase(owner, { from: owner });
+      await creator.setApprovalForAll(operator, true, { from: owner });
+
+      await creator.safeTransferFrom(owner, anyone1, 1, { from: operator });
+    });
+
+    it('should allow if operator owns token', async () => {
+      await registry.setBlockedOperators([operator], { from: owner });
+      await creator.mintBase(operator, { from: owner });
+
+      await creator.safeTransferFrom(operator, anyone1, 1, { from: operator });
     })
+
+    it('should block filtered operators', async () => {
+      await registry.setBlockedOperators([operator], { from: owner });
+      await creator.mintBase(owner, { from: owner });
+      await creator.setApprovalForAll(operator, true, { from: owner });
+
+      await truffleAssert.reverts(creator.safeTransferFrom(owner, anyone1, 1, { from: operator }));
+      await truffleAssert.reverts(creator.transferFrom(owner, anyone1, 1, { from: operator }));
+      await creator.safeTransferFrom(owner, anyone1, 1, { from: owner });
+    })
+
+    it('should return registry and subscription', async () => {
+      assert.equal(registry.address, await ext.getOperatorFiltererRegistry());
+      assert.equal('0x000000000000000000000000000000000000dEaD', await ext.getSubscription());
+    });
   });
 
   describe("ERC1155", () => {
     let creator, registry, ext;
 
     beforeEach(async () => {
-      creator = await ERC1155Creator.new("gm", "GM", { from: owner, gas: 9000000 });
+      creator = await ERC1155Creator.new("gm", "gm", { from: owner, gas: 9000000 });
       registry = await MockRegistry.new({ from: owner });
-      ext = await OperatorFilter.new(mock.address, '0x000000000000000000000000000000000000dEaD', false, { from: owner });
+      ext = await OperatorFilter.new(registry.address, '0x000000000000000000000000000000000000dEaD', { from: owner });
 
       await creator.setApproveTransfer(ext.address);
     });
 
-    it('should allow', () => { })
-    it('should allow if operator owns token', async () => { })
-    it('should block filtered operators', async () => {
-      await registry.setBlockedOperators([operator], true);
-      await creator.mintBase(owner, tokenURI, { from: owner });
-      await creator.approve(operator, 1, { from: owner });
+    it('should allow operators not on filtered list', async () => {
+      await registry.setBlockedOperators([operator2 /* <-- not `operator` */], { from: owner });
+      await creator.mintBaseNew([owner], [1], [tokenURI], { from: owner });
+      await creator.setApprovalForAll(operator, true, { from: owner });
 
-      // operator is blocked
-      await truffleAssert.reverts(creator.transferFrom(owner, anyone1, 1, { from: operator }));
-      await truffleAssert.reverts(creator.safeTransferFrom(owner, anyone1, 1, { from: operator }));
-      
-      // but owner can still transfer
-      await creator.transferFrom(owner, anyone1, 1, { from: owner });
+      await creator.safeTransferFrom(owner, anyone1, 1, 1, "0x0", { from: operator });
+    });
+
+    it('should allow if operator owns token', async () => {
+      await registry.setBlockedOperators([operator], { from: owner });
+      await creator.mintBaseNew([operator], [1], [tokenURI], { from: owner });
+
+      await creator.safeTransferFrom(operator, anyone1, 1, 1, "0x0", { from: operator });
+    })
+
+    it('should block filtered operators', async () => {
+      await registry.setBlockedOperators([operator], { from: owner });
+      await creator.mintBaseNew([owner], [1], [tokenURI], { from: owner });
+      await creator.setApprovalForAll(operator, true, { from: owner });
+
+      await truffleAssert.reverts(creator.safeTransferFrom(owner, anyone1, 1, 1, "0x0", { from: operator }));
+      await creator.safeTransferFrom(owner, anyone1, 1, 1, "0x0", { from: owner });
     })
   });
 
-  // describe('operator filter', function() {
-  //   it('should allow when empty', async function() {
-  //     const { creator, mock, ext } = await setup({ failAddresses: [] });
-
-  //     await ext.mint(creator.address, owner, tokenURI, { from: owner });
-  //     await creator.approve(operator, 1, { from: owner });
-  //     await creator.transferFrom(owner, anyone1, 1, { from: operator });
-
-  //     assert.equal(await creator.balanceOf(anyone1), 1);
-  //   })
-
-  //   it('should allow when not in registry', async function() {
-  //     const { creator, mock, ext } = await setup({ failAddresses: [operator2] });
-
-  //     await ext.mint(creator.address, owner, tokenURI, { from: owner });
-  //     await creator.approve(operator, 1, { from: owner });
-  //     await creator.transferFrom(owner, anyone1, 1, { from: operator });
-
-  //     assert.equal(await creator.balanceOf(anyone1), 1);
-  //   })
-
-  //   it('should block even when approved', async function () {
-  //     const { creator, mock, ext } = await setup({ failAddresses: [operator] });
-
-  //     await ext.mint(creator.address, owner, tokenURI, { from: owner });
-  //     await creator.approve(operator, 1, { from: owner });
-
-  //     truffleAssert.reverts(creator.transferFrom(owner, anyone1, 1, { from: operator }));
-  //     truffleAssert.reverts(creator.safeTransferFrom(owner, anyone1, 1, { from: operator }));
-  //   });
-
-  //   it('should block even when approved for all', async function () {
-  //     const { creator, mock, ext } = await setup({ failAddresses: [operator] });
-
-  //     await ext.mint(creator.address, owner, tokenURI, { from: owner });
-  //     await creator.setApprovalForAll(operator, true, { from: owner });
-
-  //     truffleAssert.reverts(creator.transferFrom(owner, anyone1, 1, { from: operator }));
-  //     truffleAssert.reverts(creator.safeTransferFrom(owner, anyone1, 1, { from: operator }));
-  //   });
-  // })
 });
