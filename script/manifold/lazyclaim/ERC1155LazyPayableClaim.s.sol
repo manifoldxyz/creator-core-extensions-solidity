80562d6944a5fbe4a35bbbed68eafd8a2188a833
ediep <dieperic@gmail.com>
2023-05-11 01:37:55 -0700

    fix typo

--- script/manifold/lazyclaim/ERC1155LazyPayableClaim.s.sol
+++ script/manifold/lazyclaim/ERC1155LazyPayableClaim.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import "../../../contracts/manifold/lazyclaim/ERC1155LazyPayableClaim.sol";
 
-contract DeployERC721LazyPayableClaim is Script {
+contract DeployERC1155LazyPayableClaim is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
