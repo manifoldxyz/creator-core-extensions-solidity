956c4b6d3f2517f2f71e0fb46e2980ea4ea33510
Wilkins Chung <wilkins.chung@gmail.com>
2023-06-09 15:47:52 -0700

    fix typos

--- packages/manifold/scripts/ERC721BurnRedeem.s.sol
+++ packages/manifold/scripts/ERC721BurnRedeem.s.sol
@@ -15,7 +15,7 @@ contract DeployERC721BurnRedeem is Script {
         // Deploy by specifying the linked library address and at 150 runs (identical to linked library)
         // e.g.
         //   forge script scripts/ERC721BurnRedeem.s.sol --optimizer-runs 150 --rpc-url <YOUR_NODE> --libraries contracts/burnredeem/BurnRedeemLib.sol:BurnRedeemLib:<BURN_REDEEM_LIB_ADDRESS> --broadcast
-        //   forge verify-contract --compiler-version 0.8.17 --optimizer-runs 150 --chain goerli <DEPLOYED_ADDRESS> contracts/burnredeem/ERC721BurnRedeem.sol:ERC721BurnRedeem --libraries contracts/burnredeem/BurnRedeemLib.sol:BurnRedeemLib:<BURN_REDEEM_LIB_ADDRESS> --constructor-args $(cast abi-encode "constructor(address)", "<INITIAL_OWNER>") --watch
+        //   forge verify-contract --compiler-version 0.8.17 --optimizer-runs 150 --chain goerli <DEPLOYED_ADDRESS> contracts/burnredeem/ERC721BurnRedeem.sol:ERC721BurnRedeem --libraries contracts/burnredeem/BurnRedeemLib.sol:BurnRedeemLib:<BURN_REDEEM_LIB_ADDRESS> --constructor-args $(cast abi-encode "constructor(address)" "<INITIAL_OWNER>") --watch
         new ERC721BurnRedeem{salt: 0x4552433732314275726e52656465656d4552433732314275726e52656465656d}(initialOwner);
         vm.stopBroadcast();
     }
