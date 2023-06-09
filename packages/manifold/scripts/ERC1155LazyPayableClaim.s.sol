60563e7b37eee2835e30605c3643aae784ef7159
Wilkins Chung <wilkins.chung@gmail.com>
2023-06-09 15:33:27 -0700

    fix remappings and dependencies

--- packages/manifold/scripts/ERC1155LazyPayableClaim.s.sol
+++ packages/manifold/scripts/ERC1155LazyPayableClaim.s.sol
@@ -14,7 +14,7 @@ contract DeployERC1155LazyPayableClaim is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
         // forge script scripts/ERC1155LazyPayableClaim.s.sol --optimizer-runs 1000 --rpc-url <YOUR_NODE> --broadcast
-        // forge verify-contract --compiler-version 0.8.17 --optimizer-runs 1000 --chain goerli <DEPLOYED_ADDRESS> contracts/lazyclaim/ERC1155LazyPayableClaim.sol:ERC1155LazyPayableClaim --constructor-args (cast abi-encode "constructor(address,address)" "<INITIAL_OWNER>" "0x00000000000076A84feF008CDAbe6409d2FE638B") --watch
+        // forge verify-contract --compiler-version 0.8.17 --optimizer-runs 1000 --chain goerli <DEPLOYED_ADDRESS> contracts/lazyclaim/ERC1155LazyPayableClaim.sol:ERC1155LazyPayableClaim --constructor-args $(cast abi-encode "constructor(address,address)" "<INITIAL_OWNER>" "0x00000000000076A84feF008CDAbe6409d2FE638B") --watch
         new ERC1155LazyPayableClaim{salt: 0x455243313135354c617a7950617961626c65436c61696d455243313135354c61}(initialOwner, DELEGATION_REGISTRY);
         vm.stopBroadcast();
     }
