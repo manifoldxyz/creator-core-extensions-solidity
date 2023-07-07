a402d94d70420c67afcb58816a31b327eb86f95f
Hieu Bui <128430554+hmbui36@users.noreply.github.com>
2023-07-07 15:14:18 -0400

    Update OperatorFilterer.s.sol

--- packages/manifold/scripts/OperatorFilterer.s.sol
+++ packages/manifold/scripts/OperatorFilterer.s.sol
@@ -14,7 +14,7 @@ contract DeployOperatorFilterer is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
         // forge script scripts/OperatorFilterer.s.sol --optimizer-runs 1000 --rpc-url <YOUR_NODE> --broadcast
-        // forge verify-contract --compiler-version 0.8.17 --optimizer-runs 1000 --chain goerli <DEPLOYED_ADDRESS> contracts/operatorfilterer/OperatorFilterer.sol:OperatorFilterer --constructor-args $(cast abi-encode "constructor(address,address)" "${SUBSCRIPTION}" "0x000000000000AAeB6D7670E522A718067333cd4E") --watch
+        // forge verify-contract --compiler-version 0.8.17 --optimizer-runs 1000 --chain goerli <DEPLOYED_ADDRESS> contracts/operatorfilterer/OperatorFilterer.sol:OperatorFilterer --constructor-args $(cast abi-encode "constructor(address,address)" "0x000000000000AAeB6D7670E522A718067333cd4E" "${SUBSCRIPTION}") --watch
         new OperatorFilterer{salt: 0x4f70657261746f7246696c74657265724f70657261746f7246696c7465726572}(OPERATOR_FILTER_REGISTRY, subscription);
 
         vm.stopBroadcast();
