5b4a808b85b723d251d3b80a2fe885d14af506c2
Campion Fellin <campionfellin@gmail.com>
2023-11-14 07:55:43 -0800

    chore: tests
    Signed-off-by: Campion Fellin <campionfellin@gmail.com>

--- packages/manifold/test/physicalclaim/PhysicalClaim.t.sol
+++ packages/manifold/test/physicalclaim/PhysicalClaim.t.sol
@@ -2501,6 +2501,106 @@ contract PhysicalClaimTest is Test {
     vm.stopPrank();
   }
 
+  function testNoBurnSpec() public {
+    vm.startPrank(owner);
+
+    // Mint 2 tokens to other
+    creatorCore721.mintBase(other, "");
+    creatorCore721.mintBase(other, "");
+
+    IPhysicalClaimCore.BurnItem[] memory burnItems = new IPhysicalClaimCore.BurnItem[](1);
+    burnItems[0] = IPhysicalClaimCore.BurnItem({
+      validationType: IPhysicalClaimCore.ValidationType.CONTRACT,
+      contractAddress: address(creatorCore721),
+      burnTokenSpec: IPhysicalClaimCore.BurnTokenSpec.ERC721_NO_BURN, // no burn!
+      burnFunctionSpec: IPhysicalClaimCore.BurnFunctionSpec.MANIFOLD,
+      amount: 1,
+      minTokenId: 1,
+      maxTokenId: 3,
+      merkleRoot: ""
+    });
+
+    IPhysicalClaimCore.BurnGroup[] memory burnSet = new IPhysicalClaimCore.BurnGroup[](1);
+    burnSet[0] = IPhysicalClaimCore.BurnGroup({
+      requiredCount: 1,
+      items: burnItems
+    });
+
+    IPhysicalClaimCore.VariationLimit[] memory variations = new IPhysicalClaimCore.VariationLimit[](1);
+    variations[0] = IPhysicalClaimCore.VariationLimit({
+      id: 1,
+      totalSupply: 10
+    });
+
+    // Create claim initialization parameters. Total supply is 1 so they will use the whole supply
+    IPhysicalClaimCore.PhysicalClaimParameters memory claimPs = IPhysicalClaimCore.PhysicalClaimParameters({
+      paymentReceiver: payable(owner),
+      totalSupply: 0,
+      startDate: 0,
+      endDate: 0,
+      burnSet: burnSet,
+      variationLimits: variations,
+      signer: vm.addr(privateKey)
+    });
+
+    // Initialize the physical claim
+    example.initializePhysicalClaim(instanceId, claimPs);
+
+    vm.stopPrank();
+    vm.startPrank(other);
+
+    // Does not need to approve token for burning
+    // creatorCore721.approve(address(example), 1);
+    // creatorCore721.approve(address(example), 2);
+
+    IPhysicalClaimCore.BurnToken[] memory burnTokens = new IPhysicalClaimCore.BurnToken[](1);
+    burnTokens[0] = IPhysicalClaimCore.BurnToken({
+      groupIndex: 0,
+      itemIndex: 0,
+      contractAddress: address(creatorCore721),
+      id: 1,
+      merkleProof: new bytes32[](0)
+    });
+
+    IPhysicalClaimCore.PhysicalClaimSubmission[] memory submissions = new IPhysicalClaimCore.PhysicalClaimSubmission[](1);
+    submissions[0].instanceId = uint56(instanceId);
+    submissions[0].count = 1;
+    submissions[0].currentClaimCount = 0;
+    submissions[0].burnTokens = burnTokens;
+    submissions[0].totalCost = 1;
+    submissions[0].variation = 1;
+    submissions[0].data = "";
+    submissions[0].nonce = "abcd";
+
+    bytes32 message = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", instanceId, uint(1)));
+
+    (uint8 v, bytes32 r, bytes32 s) = vm.sign(privateKey, message);
+    bytes memory signature = abi.encodePacked(r, s, v);
+
+    submissions[0].signature = signature;
+    submissions[0].message = message;
+
+    // Fine
+    example.burnRedeem{value: 1}(submissions);
+
+    // Verify that the token still exists..
+    assertEq(creatorCore721.ownerOf(1), other);
+
+    // Cannot re-use token
+    submissions[0].currentClaimCount = 1;
+    submissions[0].nonce = "efgh";
+    vm.expectRevert(IPhysicalClaimCore.InvalidBurnAmount.selector);
+    example.burnRedeem{value: 1}(submissions);
+
+    // Try with invalid burn redeem amount
+    burnTokens[0].id = 2;
+    submissions[0].count = 2;
+    vm.expectRevert(IPhysicalClaimCore.InvalidBurnAmount.selector);
+    example.burnRedeem{value: 1}(submissions);
+
+    vm.stopPrank();
+  }
+
   function testChangeVariationsToLower() public {
     vm.startPrank(owner);
 
