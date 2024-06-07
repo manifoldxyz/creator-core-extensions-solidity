8efe7e1a3573bd2c3f1f8d31843389da5428552c
nikki-kiga <42276368+nikki-kiga@users.noreply.github.com>
2024-06-07 13:33:31 -0700

    fix: comment out script lines on gacha (#95)

--- packages/manifold/script/ERC1155GachaLazyClaim.s.sol
+++ packages/manifold/script/ERC1155GachaLazyClaim.s.sol
@@ -18,24 +18,22 @@ import "../contracts/gachaclaims/ERC1155GachaLazyClaim.sol";
  */
 contract DeployERC1155GachaLazyClaim is Script {
     function run() external {
-        address initialOwner = 0x07297ddf5AAa3Fa3846D258EED663eb76C18D794; // uncomment this and put in your printed out wallet address based on fake pk on sepolia
-        // address initialOwner = vm.envAddress("INITIAL_OWNER"); // comment this out on sepolia
+        // address initialOwner = <your wallet address>; // uncomment this and put in your desired owner address
+        address initialOwner = vm.envAddress("INITIAL_OWNER"); // comment this out on sepolia
 
-        uint pk = 69696969696969996969996969;
-        address addr = vm.addr(pk);
-        console.log(addr);
+        // uint pk = some combo of 6s and 9s;
+        // address addr = vm.addr(pk);
+        // console.log(addr);
 
         require(initialOwner != address(0), "Initial owner address not set.  Please configure INITIAL_OWNER.");
 
-        uint256 deployerPrivateKey = pk; // uncomment this when testing on sepolia
-        // uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY"); // comment this out when testing on sepolia
+        // uint256 deployerPrivateKey = pk; // uncomment this when testing on goerli
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY"); // comment this out when testing on goerli
         vm.startBroadcast(deployerPrivateKey);
 
         //  forge script script/ERC1155GachaLazyClaim.s.sol:DeployERC1155GachaLazyClaim --optimizer-runs 1000 --rpc-url <YOUR_NODE> --broadcast
-        // forge verify-contract --compiler-version 0.8.17 --optimizer-runs 1000 --chain sepolia 0x6664775828c892d06a18cc5599bff9c7781f018f contracts/gachaclaims/ERC1155GachaLazyClaim.sol:ERC1155GachaLazyClaim --constructor-args $(cast abi-encode "constructor(address)" "${INITIAL_OWNER}") --watch
+        // forge verify-contract --compiler-version 0.8.17 --optimizer-runs 1000 --chain sepolia <DEPLOYED_ADDRESS>  contracts/gachaclaims/ERC1155GachaLazyClaim.sol:ERC1155GachaLazyClaim --constructor-args $(cast abi-encode "constructor(address)" "${INITIAL_OWNER}") --watch
         new ERC1155GachaLazyClaim{salt: 0x16091cc3cd908d7d973f650f59bd476ac79090f0358f87c50a7f5caee0835a84}(initialOwner);
         vm.stopBroadcast();
     }
 }
-
-// forge verify-contract --compiler-version 0.8.17 --optimizer-runs 1000 --chain sepolia 0xa160a0a48b6ddb9ffd01a7b85e0c2b331c912e29 contracts/gachaclaims/ERC1155GachaLazyClaim.sol:ERC1155GachaLazyClaim --constructor-args $(cast abi-encode "constructor(address)" "0x07297ddf5AAa3Fa3846D258EED663eb76C18D794") --watch
