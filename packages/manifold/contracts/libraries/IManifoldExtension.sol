160c28eb55233cfe03156290d34f6963ef6bacbe
wwhchung <wilkins.chung@gmail.com>
2024-11-21 15:46:58 -0800

    generic manifold extension mint events (#97)
    * generic manifold extension mint events
    
    * add minter

---
+++ packages/manifold/contracts/libraries/IManifoldExtension.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/**
+ * @title IManifoldExtension
+ * @notice Interface for Manifold extensions. It defines all the events that an extension will emit whenever a token is minted.
+ */
+interface IManifoldExtension {
+    event InstanceMint(
+        address creatorContractAddress,
+        uint256 indexed instanceId,
+        address minter,
+        address indexed tokenAddress,
+        uint256 indexed tokenId,
+        uint256 quantity
+    );
+    event InstanceBatchMint(
+        address creatorContractAddress,
+        uint256 indexed instanceId,
+        address minter,
+        address indexed tokenAddress,
+        uint256[] indexed tokenIds,
+        uint256[] quantity
+    );
+}
